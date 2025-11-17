5ab4cefbc27656554e59cfcba930f06c8bc773fb
Triet Do <trietdeptraingu@gmail.com>
2025-11-17 20:53:34 +0700

    Commiting a syntax error for grafana

--- restaurant/src/config.js
+++ restaurant/src/config.js
@@ -1,2 +1,6 @@
 // export const url = "http://localhost:5000";
 export const url = "https://tt-cnpm.onrender.com";
+
+// Intentional syntax error to test CI/CD error handling
+const brokenCode = {
+  missing: "closing brace"
